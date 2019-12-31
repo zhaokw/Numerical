2cca21ce3acf5fd9dea8bbe6baee19af8bef11cb
unknown <zhaokw@outlook.com>
2019-12-31 18:20:59 -0500

    small fixes

--- PolyRegression.m
+++ PolyRegression.m
@@ -14,14 +14,14 @@ hold on;
 	
 	p = 1.5;
 	qInitial = randn(d,1); 
-	[q,ne,fe] = findRootDEp(qInitial,Xs,Ys,p);
+	[q,~,fe] = findRootDEp(qInitial,Xs,Ys,p);
 	DATA_i = linspace(min(Xs),max(Xs),4*N);
 	plot(DATA_i,polyval(q(end:-1:1),DATA_i),'b-');
 	fprintf('blue: p=%0.2f, y=%0.2fx^3+(%0.2f)x^2+(%0.2f)x+(%0.2f), err=%0.2f', p, q(end:-1:1), fe);
 
 	p = 2.00;
 	qInitial = randn(d,1); 
-	[q,ne,fe] = findRootDEp(qInitial,Xs,Ys,p);
+	[q,~,fe] = findRootDEp(qInitial,Xs,Ys,p);
 	DATA_i = linspace(min(Xs),max(Xs),4*N);
 	plot(DATA_i,polyval(q(end:-1:1),DATA_i),'g-');
     fprintf(newline)
@@ -29,7 +29,7 @@ hold on;
 
 	p = 5.0;
 	qInitial = randn(d,1); 
-	[q,ne,fe] = findRootDEp(qInitial,Xs,Ys,p);
+	[q,~,fe] = findRootDEp(qInitial,Xs,Ys,p);
 	DATA_i = linspace(min(Xs),max(Xs),4*N);
 	plot(DATA_i,polyval(q(end:-1:1),DATA_i),'r-');
     fprintf(newline)
@@ -39,38 +39,38 @@ hold on;
 hold off;
 
 % Gets the p-error of the simulating polynomial
-function result = Ep(q,x,y,p);
+function result = Ep(q,x,y,p)
     result = sum(abs(polyval(q(end:-1:1),x)-y).^p);
 end
 
 % Gets the first derivative of Ep
-function result = dEp(q,x,y,p);
+function result = dEp(q,x,y,p)
     r = polyval(q(end:-1:1),x)-y;
     result = zeros(length(q),1);
-    for j=0:length(q)-1;
+    for j=0:length(q)-1
         result(1+j) = sum(p*(abs(r).^(p-1)).*(x.^j).*sign(r));
-    end;
+    end
 end
 
 % Gets the second derivates of Ep
 function result = ddEp(q,x,y,p)
     r = polyval(q(end:-1:1),x)-y;
     result = zeros(length(q),length(q));
-    for j=0:length(q)-1;
-        for k=0:length(q)-1;
+    for j=0:length(q)-1
+        for k=0:length(q)-1
             result(1+j,1+k) = sum(p*(p-1)*(abs(r).^(p-2)).*(x.^(j+k)));
-        end;%for k=0:length(q)-1;
+        end
     end
 end
 
 % Gets optimal polynomial minimizing Ep
-function [qroot,newtonerror,finalerror] = findRootDEp(qInitial,x,y,p);
+function [qroot,newtonerror,finalerror] = findRootDEp(qInitial,x,y,p)
     maxItr = 15; itr = 0; q = qInitial;
-    while (itr < maxItr);
-        dq = inv(ddEp(q,x,y,p))*dEp(q,x,y,p);
+    while (itr < maxItr)
+        dq = ddEp(q,x,y,p)\dEp(q,x,y,p);
         q = q - dq;
         itr = itr+1;
-    end; %while;
+    end
     qroot = q; newtonerror = norm(dEp(qroot,x,y,p)); finalerror = Ep(qroot,x,y,p);
 end
 
